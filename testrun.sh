432ecac3a57f457eee3140838a91bc7a682e8db6
K. Posern <k.posern@gmail.com>
2017-09-05 16:08:03 +0200

    added testrun.sh script + Makefile target to run it + Makefile target to reinstall (so install without config file)

---
+++ testrun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CONFFILE="/etc/ykfde.conf"
+[ -e "src/hooks/ykfde" ] || { echo "ERROR: src/hooks/ykfde not found."; exit 1; }
+. "$CONFFILE"
+[ -z "$YKFDE_LUKS_NAME" ] && { echo "ERROR: YKFDE_LUKS_NAME not set (check '$CONFFILE')."; exit 1; }
+[ -e "/dev/mapper/$YKFDE_LUKS_NAME" ] && cryptsetup luksClose "$YKFDE_LUKS_NAME"
+. src/hooks/ykfde
+run_hook
