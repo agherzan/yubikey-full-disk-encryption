8170de8a5d958b2cbbd8f96c7fd64037e99521b3
Vincent43 <31109921+Vincent43@users.noreply.github.com>
2018-07-13 14:59:06 +0200

    Minor code beautyfication

--- testrun.sh
+++ testrun.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 CONFFILE="/etc/ykfde.conf"
-[ -e "src/hooks/ykfde" ] || { echo "ERROR: src/hooks/ykfde not found."; exit 1; }
+[ -e "src/hooks/ykfde" ] || {
+  echo "ERROR: src/hooks/ykfde not found."
+  exit 1
+}
 . "$CONFFILE"
-[ -z "$YKFDE_LUKS_NAME" ] && { echo "ERROR: YKFDE_LUKS_NAME not set (check '$CONFFILE')."; exit 1; }
+[ -z "$YKFDE_LUKS_NAME" ] && {
+  echo "ERROR: YKFDE_LUKS_NAME not set (check '$CONFFILE')."
+  exit 1
+}
 [ -e "/dev/mapper/$YKFDE_LUKS_NAME" ] && cryptsetup luksClose "$YKFDE_LUKS_NAME"
 . src/hooks/ykfde
 run_hook
